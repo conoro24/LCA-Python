0f9dcadd4c6a92aa611385a60f757e191e00eaa4
conoro24 <conorbrian99@gmail.com>
2020-11-14 14:10:00 +0000

    Dag Branch

--- LCA2_unittest.py
+++ LCA2_unittest.py
@@ -1,17 +1,64 @@
-import LCA2
-import unittest
+from LowestCommonAncestorDAG import Node, dagLCA
 
-class TestLCA2(unittest.TestCase):
+class TestLowestCommonAncestorDAG:
+    def test_simple(self):
+        root = Node(1)
+        r2 = Node(2)
+        r3 = Node(3)
+        r4 = Node(4)
+        r5 = Node(5)
+        r6 = Node(6)
+        root.succ = [r2,r3,r4,r5]
+        r2.succ = [r4]
+        r2.pred = [root]
+        r3.succ = [r4, r5]
+        r3.pred = [root]
+        r4.succ = [r5]
+        r4.pred = [r2,r3,root]
+        r5.pred = [r3,r4,root]
+        r6.pred = [r4]
 
-    def test_1(self):
-        self.assertEqual(LCA2.lca(LCA2.root, 4, 5), 2)
+        lca = dagLCA(root, r4, r5)
 
-    def test_2(self):
-        self.assertEqual(LCA2.lca(LCA2.root, 3, 4), 1)
+        assert lca is 3
 
-    def test_3(self):
-        self.assertEqual(LCA2.lca(LCA2.root, 6, 8), 3)
+    def test_null(self):
+        root = None
 
+        lca = dagLCA(root, 4, 5)
 
-if __name__ == '__main__':
-    unittest.main()
+        assert lca is None
+
+    def test_root(self):
+        root = Node(1)
+        r2 = Node(2)
+        r3 = Node(3)
+        root.succ = [r2,r3]
+
+        lca = dagLCA(root, root, r3)
+        assert lca is 1
+        lca = dagLCA(root, r3, root)
+        assert lca is 1
+        lca = dagLCA(root, r2, r2)
+        assert lca is 2
+
+    def test_no_immediate_lca(self):
+        root = Node(1)
+        r2 = Node(2)
+        r3 = Node(3)
+        r4 = Node(4)
+        r5 = Node(5)
+        r6 = Node(6)
+        root.succ = [r2,r3]
+        r2.succ = [r4]
+        r2.pred = [root]
+        r3.succ = [r4, r5]
+        r3.pred = [root]
+        r4.succ = [r5]
+        r4.pred = [r2]
+        r5.pred = [r3]
+        r6.pred = [r4]
+
+        lca = dagLCA(root, r4, r5)
+
+        assert lca is 1
